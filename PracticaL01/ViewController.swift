4409dccdd11880521a3a0e9a2d819560516f74cd
Donatto <grenwket@gmail.com>
2022-04-21 15:07:00 -0700

    Initial Commit

---
+++ PracticaL01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticaL01
+//
+//  Created by Donatto on 21/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
